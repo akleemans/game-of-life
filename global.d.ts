ddb637bda5c36661127fd2c0405a6dbca19ac255
Adrianus Kleemans <a.kleemans@gmail.com>
2021-03-19 19:59:40 +0100

    Work on resetting & random state, reformatting

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,6 @@
 // This file will add both p5 instanced and global intellisense
 import module = require('p5');
-import * as p5Global from 'p5/global'
+import 'p5/global'
 
 export = module;
 export as namespace p5;
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,6 +1,6 @@
 enum State {
-  input,
-  running
+    input,
+    running
 }
 
 const cellsA: number[][] = [];
@@ -8,110 +8,105 @@ const cellsB: number[][] = [];
 let activeCells = cellsA;
 let state: State = State.input;
 
-const w = 80;
-const h = 60;
-const cellSize = 20;
+const w = 45;
+const h = 30;
+const cellSize = 15;
+const filledFieldFactor = 0.15;
 
-// P5 will automatically use global mode if a draw() function is defined
 function setup() {
-  const myCanvas = createCanvas(800, 600);
-  myCanvas.parent('canvasDiv');
+    console.log("🚀 - Setup initialized - P5 is running");
+    const myCanvas = createCanvas(w * cellSize, h * cellSize);
+    myCanvas.parent('canvasDiv');
+    frameRate(10);
 
-  for (let i = 0; i < h; i++) {
-    const newCellsA = [];
-    const newCellsB = [];
-    for (let j = 0; j < w; j++) {
-      newCellsA.push(0);
-      newCellsB.push(0); // Math.random() < 0.3 ? 1 : 0
+    for (let i = 0; i < w; i++) {
+        const newCellsA = [];
+        const newCellsB = [];
+        for (let j = 0; j < h; j++) {
+            newCellsA.push(Math.random() < filledFieldFactor ? 1 : 0)
+            newCellsB.push(0);
+        }
+        cellsA.push(newCellsA);
+        cellsB.push(newCellsB);
     }
-    cellsA.push(newCellsA);
-    cellsB.push(newCellsB);
-  }
-
-  // console.log("🚀 - Setup initialized - P5 is running");
-  // createCanvas(windowWidth, windowHeight);
-  // rectMode(CENTER).noFill().frameRate(30);
-
-  // Speed slider
-  // speed = createSlider(0, 15, 3, 1);
-  // speed.position(10, 10);
-  // speed.style("width", "80px");
 }
 
 function draw() {
-  // Calculate next gen
-  if (state === State.running && frameCount % 15 === 0) {
-    calculateNextGen();
-  }
+    // Calculate next gen
+    if (state === State.running && frameCount % 5 === 0) {
+        calculateNextGen();
+    }
 
-  // Draw
-  drawOnCanvas()
+    // Draw
+    drawOnCanvas()
 }
 
 let calculateNextGen = () => {
-  // Switch arrays
-  let oldCells;
-  if (activeCells === cellsA) {
-    activeCells = cellsB;
-    oldCells = cellsA;
-  } else {
-    activeCells = cellsA;
-    oldCells = cellsB;
-  }
-
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j < w; j++) {
-      if (countNeighbors(i, j, oldCells) === 3 || (oldCells[i][j] === 1 && countNeighbors(i, j, oldCells) === 2)) {
-        activeCells[i][j] = 1;
-      } else {
-        activeCells[i][j] = 0;
-      }
+    // Switch arrays
+    let oldCells;
+    if (activeCells === cellsA) {
+        activeCells = cellsB;
+        oldCells = cellsA;
+    } else {
+        activeCells = cellsA;
+        oldCells = cellsB;
     }
-  }
+
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h; j++) {
+            if (countNeighbors(i, j, oldCells) === 3 || (oldCells[i][j] === 1 && countNeighbors(i, j, oldCells) === 2)) {
+                activeCells[i][j] = 1;
+            } else {
+                activeCells[i][j] = 0;
+            }
+        }
+    }
 }
 
 const countNeighbors = (i: number, j: number, oldCells: number[][]): number => {
-  let count = 0;
-  if (j === 0 || i === 0 || j === w - 1 || i === h - 1) {
-    return 0;
-  }
-  count += oldCells[i - 1][j - 1];
-  count += oldCells[i - 1][j];
-  count += oldCells[i - 1][j + 1];
+    return getN(oldCells, i - 1, j - 1) + getN(oldCells, i - 1, j) + getN(oldCells, i - 1, j + 1)
+        + getN(oldCells, i, j - 1) + getN(oldCells, i, j + 1)
+        + getN(oldCells, i + 1, j - 1) + getN(oldCells, i + 1, j) + getN(oldCells, i + 1, j + 1);
+}
 
-  count += oldCells[i][j - 1];
-  count += oldCells[i][j + 1];
-
-  count += oldCells[i + 1][j - 1];
-  count += oldCells[i + 1][j];
-  count += oldCells[i + 1][j + 1];
-  return count;
+const getN = (oldCells: number[][], i: number, j: number): number => {
+    return (oldCells[i] ?? [])[j] ?? 0;
 }
 
 const drawOnCanvas = (): void => {
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j < w; j++) {
-      const value = activeCells[i][j];
-      fill(value === 1 ? 0 : 250);
-      square(i * cellSize, j * cellSize, cellSize);
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h; j++) {
+            const value = activeCells[i][j];
+            fill(value === 1 ? 0 : 250);
+            square(i * cellSize, j * cellSize, cellSize);
+        }
     }
-  }
 }
 
-const toggleRun = () => {
-  if (state === State.running) {
-    state = State.input;
-  } else {
-    state = State.running;
-  }
+const toggleRun = (): void => {
+    state = (state === State.running) ? State.input : State.running;
 };
 
-function mouseClicked(event: any) {
-  console.log('mouseClicked:', event);
-  const x = Math.floor(event.pageX / 20);
-  const y = Math.floor(event.pageY / 20);
+const resetField = (random: boolean): void => {
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h; j++) {
+            let value = 0;
+            if (random) {
+                value = Math.random() < filledFieldFactor ? 1 : 0;
+            }
+            cellsA[i][j] = value;
+            cellsB[i][j] = value;
+        }
+    }
+};
 
-  if (state === State.input) {
-    activeCells[x][y] = 1;
-  }
+// Triggered by P5
+function mouseClicked(event: any): void {
+    console.log('mouseClicked:', event);
+    const x = Math.floor(event.layerX / cellSize);
+    const y = Math.floor(event.layerY / cellSize);
+
+    if (state === State.input && x >= 0 && x < w && y > 0 && y < h) {
+        activeCells[x][y] = (activeCells[x][y] === 0) ? 1 : 0;
+    }
 }
